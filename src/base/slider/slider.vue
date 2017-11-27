e1ae59c8d8fb49e8c6bfb16452e7f3abbf84128c
Artila <529276960@qq.com>
2017-11-27 23:50:16 +0800

    轮播图

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="slider" ref = "slider">
+    <div class="slider-group" ref = "sliderGroup">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .slider
+    min-height:1px
+    .slider-group
+      position: relative
+      overflow: hidden
+      white-space: nowrap
+      a
+        display: block
+        width: 100%
+        overflow: hidden
+        text-decoration: none
+      img
+        display: block
+        width: 100%
+</style>
