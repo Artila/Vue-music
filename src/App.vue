23ccd573b68ae920d16a36413dada5c6922fac7c
Artila <529276960@qq.com>
2017-11-28 23:47:05 +0800

    keep-alive

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
   <div id="app">
     <m-header></m-header>
     <tab></tab>
-    <router-view></router-view>
+    <!--<keep-alive> 包裹动态组件时，会缓存不活动的组件实例，而不是销毁它们。
+    和 <transition> 相似，<keep-alive> 是一个抽象组件：它自身不会渲染一个 DOM 元素，也不会出现在父组件链中。-->
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
   </div>
 </template>
 
