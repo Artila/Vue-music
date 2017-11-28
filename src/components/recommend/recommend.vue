d007ed488d7d19d1364b26845097971088411072
Artila <529276960@qq.com>
2017-11-28 23:46:56 +0800

    推荐页

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="recommend">
-  	<div class="slider-wrapper">
+  	<div v-if="recommends.length" class="slider-wrapper">
       <slider>
         <div v-for = "item in recommends">
           <a :href="item.linkUrl">
@@ -30,7 +30,7 @@
 			this._getRecommend()
 		},
 		methods: {
-			_getRecommend() {
+			_getRecommend() { // 异步操作，渲染时可能数据还没获得，所以在渲染前要判断数据是否获得
 				getRecommend().then((res) => {
 					if (res.code === ERR_OK) {
 						this.recommends = res.data.slider
@@ -45,10 +45,22 @@
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-	.recommend
-		position: fixed
-		width: 100%
-		top: 88px
-		bottom: 0
+  @import "~common/stylus/variable"
 
+	.recommend
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .slider-wrapper
+      position: relative
+      width: 100%
+      overflow: hidden
+    .recommend-list
+      .list-title
+        height: 65px
+        line-height: 65px
+        text-align: center
+        font-style: $font-size-medium
+        color: $color-theme
 </style>
